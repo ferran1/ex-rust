23904b16edec1624d68b92cdf74aadef51c7fe68
ferran tombal <ferran.tombal@hva.nl>
2020-04-01 23:00:03 +0200

    scala types

---
+++ src/bin/scalar_types.rs
@@ -0,0 +1,36 @@
+fn main (){
+
+    let a = 30; // i = signed integer
+    let b = 35; // u = unsigned integer
+
+    // Different Integer types
+/* Length	Signed	Unsigned
+   8-bit	i8	    u8
+   16-bit	i16	    u16
+   32-bit	i32	    u32
+   64-bit	i64	    u64
+   128-bit	i128	u128
+   arch	    isize	usize */ // isize and usize depends on the computer the program is running on (64 bits for 64-bit architecture and 32 bits for 32-bit architecture)
+
+    // Floating-Point Types
+    let c = 2.0; // default type = f64
+    let d: f32 = 20.5;
+
+    // addition
+    let sum = 5 + 10;
+    // subtraction
+    let difference = 95.5 - 4.3;
+    // multiplication
+    let product = 4 * 30;
+    // division
+    let quotient = 56.7 / 32.2;
+    // remainder
+    let remainder = 43 % 5;
+
+    // boolean
+    let e = true;
+
+    // char
+    let f = 'F';
+
+}
