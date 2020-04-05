934ea977396b46ea3517d4053c50db22bb86d1d3
ferran tombal <ferran.tombal@hva.nl>
2020-04-05 23:20:15 +0200

    match

---
+++ src/bin/match.rs
@@ -0,0 +1,23 @@
+fn main () {
+
+    let number = 1;
+    let name = "Ferran";
+
+    match number {
+        1 => println!("It's number one!"),
+        2 => println!("It's number two!"),
+        1...5 => println!("It's one from 1 to 5"),
+        _ => println!("It doesn't match")
+    }
+
+    match name {
+        "Ferran" => println!("Hello Ferran!"),
+        "James" => { // scope for match arm with multiple lines
+            if number > 0 {
+                println!("Hello James, the number is bigger than zero")
+            }
+        },
+        _ => println!("Who are you?")
+    }
+
+}
